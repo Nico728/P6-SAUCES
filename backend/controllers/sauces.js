2e8cf66769972056e956be6385f065f58d9e043f
Nicolas Séau <seau.nicolas@gmail.com>
2023-02-08 19:01:21 +0100

    fin projet

--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -81,12 +81,14 @@ exports.likeDislike = (req, res, next) => {
       .then(() => res.status(200).json({ message: 'Sauce appréciée' }))
       .catch((error) => res.status(400).json({ error}));
 
-  } else if(like === -1) { // + 1 Dislike
+  } 
+  else if(like === -1) { // + 1 Dislike
       Sauce.updateOne({_id: req.params.id}, { $inc: { dislikes: 1}, $push: { usersDisliked: req.body.userId}, _id: req.params.id })
       .then(() => res.status(200).json({ message: 'Sauce non appréciée' }))
       .catch((error) => res.status(400).json({ error}));
 
-  } else {    // Annulation Like ou Dislike
+  } 
+  else {    // Annulation Like ou Dislike
       Sauce.findOne( {_id: req.params.id})
       .then( sauce => {
           if( sauce.usersLiked.indexOf(req.body.userId)!== -1){ // Annulation Like
